3f25dadf1355edf0bcca2c23b81b965dc743ddc9
mohamedmansy316 <mohamedmansy855@gmail.com>
2024-06-10 18:45:40 +0300

    update cache table

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -35,7 +35,11 @@ return new class() extends Migration {
 
         Schema::create('failed_jobs', function (Blueprint $table): void {
             $table->id();
-            $table->string('uuid', 191)->unique()->change(); // Specify the length as 191
+            // Add the uuid column
+            $table->string('uuid', 36)->nullable()->after('id');
+
+            // Add a unique constraint on the uuid column
+            $table->unique('uuid', 'failed_jobs_uuid_unique');
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
