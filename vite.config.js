6843ea03a2977d72f44e0ecce4cf85457ac33d10
mohamedmansy316 <mohamedmansy855@gmail.com>
2024-06-11 17:56:34 +0300

    add localization

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/scss/style.scss', 'resources/js/main.js'],
+            input: ['resources/scss/style.scss','resources/scss/admin/main.scss', 'resources/js/main.js'],
             refresh: true,
         }),
     ],
